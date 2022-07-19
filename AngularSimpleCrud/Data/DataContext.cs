2e8b1274ff6571371525d487babcda78f1548aa1
m-komilov <muhammadabdulloh.uzinfocom@gmail.com>
2022-07-19 17:37:18 +0500

    Add project files.

---
+++ AngularSimpleCrud/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AngularSimpleCrud.Data
+{
+    public class DataContext : DbContext
+    {
+#pragma warning disable
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+#pragma warning restore
+        public DbSet<Inspection> Inspections { get; set; }
+        public DbSet<InspectionType> InspectionTypes { get; set; }
+        public DbSet<Status> Statuss { get; set; }
+    }
+}
